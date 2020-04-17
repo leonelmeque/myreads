e7d0069a513edeb2d1be7f400c0ee027f5554870
Leonel <leomeque@gmail.com>
2020-04-18 01:27:37 +0200

    fix: Want To read Layout and Books displayed

--- src/App.js
+++ src/App.js
@@ -1,64 +1,80 @@
 import React from "react";
-import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from "./BooksAPI";
 import "./App.css";
-
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 
 //Returning the Want TO Read View
-function WantToReadPage(props) {
-  let { books } = props;
-  return (
-    <>
-      {books.map((book) => (
-        <div className="list-book-description" key={book.id}>
-          {console.log(book.id)}
-          <div className="book">
-            <div className="book-top">
-              <div
-                className="book-cover"
-                style={{
-                  width: "100%",
-                  height: 193,
-                  backgroundRepeat: "non-repeat",
-                  backgroundSize: "100% 100%",
+class WantToReadPage extends React.Component {
+  render() {
+    const { books, changeShelf } = this.props;
+    return (
+      <>
+        {books.map((book) => (
+          <div key={book.id}>
+            {book.shelf !== "wantToRead" ? (
+              <></>
+            ) : (
+              <div className="list-book-description" key={book.id}>
+                <div className="book">
+                  <div className="book-top">
+                    <div
+                      className="book-cover"
+                      style={{
+                        width: "100%",
+                        height: 193,
+                        backgroundRepeat: "non-repeat",
+                        backgroundSize: "100% 100%",
 
-                  backgroundImage: `url("${book.imageLinks.smallThumbnail}")`,
-                }}
-              />
-              <div className="book-shelf-changer">
-                <select>
-                  <option value="move" disabled>
-                    Move to...
-                  </option>
-                  <option value="currentlyReading">Currently Reading</option>
-                  <option value="wantToRead">Want to Read</option>
-                  <option value="read">Read</option>
-                  <option value="none">None</option>
-                </select>
+                        backgroundImage: `url("${
+                          book.imageLinks.smallThumbnail
+                        }")`,
+                      }}
+                    />
+                    <div className="book-shelf-changer">
+                      <select onChange={(e)=>{changeShelf(book.id,e.target.value)}}>
+                        <option value="move" disabled>
+                          Move to...
+                        </option>
+                        <option value="currentlyReading">
+                          Currently Reading
+                        </option>
+                        <option  value="wantToRead">Want to Read</option>
+                        <option value="read">Read</option>
+                        <option value="none">None</option>
+                      </select>
+                    </div>
+                  </div>
+                </div>
+                <div>
+                  <h3>
+                      {book.title}
+                  </h3>
+                  <div className="book-title">Year: {book.publishedDate}</div>
+                  
+                  <div className="book-authors">Author: {book.authors}</div>
+                  <div className="text-box">
+                    <p>{book.description}</p>
+
+                    {book.description.length < 550 ? (
+                      <></>
+                    ) : (
+                      <p className="read-more">
+                        <a href="/">See More</a>
+                      </p>
+                    )}
+                  </div>
+                </div>
               </div>
-            </div>
+            )}
           </div>
-          <div>
-            <div className="book-title">Year: {book.publishedDate}</div>
-            <div className="book-authors">Author: {book.authors}</div>
-            <div className="text-box">
-              <p>{book.description}</p>
-
-              {book.description.length < 550 ? (
-                <></>
-              ) : (
-                <p className="read-more">
-                  <a href="/">See More</a>
-                </p>
-              )}
-            </div>
-          </div>
-        </div>
-      ))}
-    </>
-  );
+        ))}
+      </>
+    );
+  }
 }
 
 function CurrentlyReading(props) {
+  const { books } = props;
   return (
     <>
       <div className="">Currently Reading</div>
@@ -80,11 +96,6 @@ class BooksApp extends React.Component {
     this.state = {
       showSearchPage: false,
       books: [],
-      user: {
-        current: [],
-        want: [],
-        read: [],
-      },
     };
   }
 
@@ -96,20 +107,34 @@ class BooksApp extends React.Component {
     });
   }
 
+  changeShelf= (id, newShelf)=> {
+    this.setState((state) => ({
+      books: state.books.map((book) => {
+        if (book.id === id) {
+          console.log("Book changed to " + newShelf);
+          book.shelf = newShelf;
+          return book;
+        }
+        return book;
+      }),
+    }));
+  }
+
   render() {
     return (
-      <div className="app">
-        {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <button
-                className="close-search"
-                onClick={() => this.setState({ showSearchPage: false })}
-              >
-                Close
-              </button>
-              <div className="search-books-input-wrapper">
-                {/*
+      <Router>
+        <div className="app">
+          {this.state.showSearchPage ? (
+            <div className="search-books">
+              <div className="search-books-bar">
+                <button
+                  className="close-search"
+                  onClick={() => this.setState({ showSearchPage: false })}
+                >
+                  Close
+                </button>
+                <div className="search-books-input-wrapper">
+                  {/*
                   NOTES: The search from BooksAPI is limited to a particular set of search terms.
                   You can find these search terms here:
                   https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
@@ -117,34 +142,69 @@ class BooksApp extends React.Component {
                   However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                   you don't find a specific author or title. Every search is limited by search terms.
                 */}
-                <input type="text" placeholder="Search by title or author" />
+                  <input type="text" placeholder="Search by title or author" />
+                </div>
+              </div>
+              <div className="search-books-results">
+                <ol className="books-grid" />
               </div>
             </div>
-            <div className="search-books-results">
-              <ol className="books-grid" />
-            </div>
-          </div>
-        ) : (
-          <>
-          
-          <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="page-container">
-              <div className="page-side-nav">
-                <ol>
-                  <li>Want To Read</li>
-                  <li>Currently Reading</li>
-                  <li>Read</li>
-                </ol>
+          ) : (
+            <>
+              <div className="list-books-title">
+                <h1>MyReads</h1>
               </div>
-              <div className="page-main-content">
-                <WantToReadPage books={this.state.books} />
+              <div className="page-container">
+                <div className="page-side-nav">
+                  <ol>
+                    <li>
+                      <Link to="/">Want To Read</Link>
+                    </li>
+                    <li>
+                      <Link to="/currentbooks">Currently Reading</Link>
+                    </li>
+                    <li>
+                      <Link to="/read">Read</Link>
+                    </li>
+                  </ol>
+                </div>
+                <div className="page-main-content">
+                  <Switch>
+                    <Route
+                      exact
+                      path="/"
+                      component={() => (
+                        <WantToReadPage
+                          books={this.state.books}
+                          changeShelf={this.changeShelf}
+                        />
+                      )}
+                    />
+                    <Route
+                      path="/currentbooks"
+                      component={() => (
+                        <CurrentlyReading
+                          books={this.state.books}
+                          changeShelf={this.changeShelf}
+                        />
+                      )}
+                    />
+                    <Route
+                      path="/read"
+                      component={() => (
+                        <Read
+                          books={this.state.books}
+                          changeShelf={this.changeShelf}
+                        />
+                      )}
+                    />
+                  </Switch>
+                </div>
               </div>
-            </div>
-          </>
-        )}
-      </div>
+            </>
+          )}
+        </div>
+      </Router>
     );
   }
 }
